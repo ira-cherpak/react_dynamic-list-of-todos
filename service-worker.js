a2f0175ea308674122d0210cc778e39c456e5eb6
Iryna Cherpak <cherpak.iryna@gmail.com>
2021-01-27 22:23:40 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.69c980133d8746ec7592bc9413534b1f.js"
+  "./precache-manifest.5cd9f59c3e64785b60ca37966787bfdd.js"
 );
 
 self.addEventListener('message', (event) => {
